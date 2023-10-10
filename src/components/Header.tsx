8155dff130b70d408c9446c4bdd276c2cb6326e7
DustyFlower <lukashevanatalia1992@gmail.com>
2023-10-10 19:04:29 +0300

    Commit 3

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg"/>
+        </header>
+    )
+
+}
